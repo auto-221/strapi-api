035e26196a0df7a0c4ff8884e86b2e9652902044
jamil laye mbaye <jamillaye@gmail.com>
2021-05-26 03:50:51 +0200

    api

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'ef214d899f250d63b1a0d57afc782243'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '0a53c0ed-3560-42b2-a304-b2133973a94e'
+};
